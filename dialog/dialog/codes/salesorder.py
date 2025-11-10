0355f06defb07db988fe0abe8f33803d3cf09ce7
amnuoo <ameenahsan.blvk@gmail.com>
2025-11-10 12:01:26 +0000

    backend

--- dialog/dialog/codes/salesorder.py
+++ dialog/dialog/codes/salesorder.py
@@ -6,9 +6,8 @@ def save_feedback(sales_order, feedback):
     if not feedback or not feedback.strip():
         frappe.throw(_("Feedback is required before submitting."))
 
-    so = frappe.get_doc("Sales Order", sales_order)
+        so = frappe.get_doc("Sales Order", sales_order)
 
-    # Make sure the fieldname below matches your child table fieldname in Sales Order
     child = so.append("feedback_table", {})
     child.feedback = feedback
 
