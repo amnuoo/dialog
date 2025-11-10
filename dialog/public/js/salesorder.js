a1a336d3a07e0a483c9c24c10beb9b9a0dc10278
amnuoo <ameenahsan.blvk@gmail.com>
2025-11-10 12:15:19 +0000

    added error

--- dialog/public/js/salesorder.js
+++ dialog/public/js/salesorder.js
@@ -5,3 +5,31 @@ frappe.ui.form.on('Sales Order', {
         });
     }
 });
+frappe.ui.form.on('Sales Order', {
+    refresh(frm) {
+        frm.add_custom_button('Add Feedback', () => {
+            let d = new frappe.ui.Dialog({
+                title: 'Add Feedback',
+                fields: [
+                    {
+                        label: 'Feedback',
+                        fieldname: 'feedback',
+                        fieldtype: 'Small Text',
+                    }
+                ],
+                primary_action_label: 'Submit',
+                primary_action(values) {
+                    if (!values.feedback || values.feedback.trim() === "") {
+                        frappe.throw(__('Please enter feedback before submitting.'));
+                        return;
+                    }
+
+                    frappe.msgprint(__('Feedback: ') + values.feedback);
+                    d.hide();
+                }
+            });
+
+            d.show();
+        });
+    }
+});
